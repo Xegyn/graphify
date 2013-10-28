dcf8092109cba966a11e66933b0ef57bac38abbb
Kristen Sundquist <kristen@csc8.com>
2013-10-27 21:54:48 -0700

    Add axis

--- graphify.js
+++ graphify.js
@@ -3,28 +3,52 @@ function Graphify (selector, data, options) {
   this.data = data;
   this.options = options;
   this.xmlns = "http://www.w3.org/2000/svg";
+  this.width = 300;
+  this.height = 200;
 }
 
-Graphify.prototype.points = function() {
+Graphify.prototype.points = function () {
   return "20,20 40,25 60,40 80,120 120,140 200,180";
 };
 
 Graphify.prototype.render = function () {
-  var svgDocument = document.createElementNS(this.xmlns, "svg");
-  svgDocument.setAttribute('xmlns', this.xmlns);
-  svgDocument.setAttribute('version', 1.1);
+  this.createSvg();
+  this.renderAxis();
+  this.renderLineGraph();
+}
+
+Graphify.prototype.createSvg = function () {
+  this.svgDocument = document.createElementNS(this.xmlns, "svg");
+  this.svgDocument.setAttribute('xmlns', this.xmlns);
+  this.svgDocument.setAttribute('version', 1.1);
+  $(this.selector).append(this.svgDocument);
+}
+
+Graphify.prototype.renderAxis = function () {
+  this.renderLine(0, 0, 0, this.height);
+  this.renderLine(0, this.height, this.width, this.height);
+}
+
+Graphify.prototype.renderLine = function (x1, y1, x2, y2) {
+  var line = document.createElementNS(this.xmlns, 'line');
+  line.setAttribute('x1', x1);
+  line.setAttribute('y1', y1);
+  line.setAttribute('x2', x2);
+  line.setAttribute('y2', y2);
+  line.setAttribute("style", "fill:none; stroke:black; stroke-width:3");
+  this.svgDocument.appendChild(line);
+}
+
+Graphify.prototype.renderLineGraph = function () {
   var polyline = document.createElementNS(this.xmlns, 'polyline');
   polyline.setAttribute('points', this.points());
   polyline.setAttribute("style", "fill:none; stroke:black; stroke-width:3");
-  svgDocument.appendChild(polyline);
-  $(this.selector).append(svgDocument);
+  this.svgDocument.appendChild(polyline);
 }
 
 
 
-
 //----------------------------------------------------
-
 
 var create_graph = function () {
   var data = [{"date" : "10/30/13", "amount" : 12.30},
@@ -36,6 +60,5 @@ var create_graph = function () {
   var graph = new Graphify("div#graph", data, {});
   graph.render();
 };
-
 
 $(create_graph)
