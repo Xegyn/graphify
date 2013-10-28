79f0dbca9a0f6127d8cccbb5b4534297c6fdd658
Kristen Sundquist <kristen@csc8.com>
2013-10-27 18:18:56 -0700

    Add function to update points in the graph

--- graphify.js
+++ graphify.js
@@ -1,8 +1,16 @@
 var data = [{"date" : "10/30/13", "amount" : 12.30},
-        {"date" : "09/30/13", "amount" : 14.30},
-        {"date" : "08/30/13", "amount" : 25.30},
-        {"date" : "07/30/13", "amount" : 17.30},
-        {"date" : "06/30/13", "amount" : 20.30},
-        {"date" : "05/30/13", "amount" : 5.30}]
+            {"date" : "09/30/13", "amount" : 14.30},
+            {"date" : "08/30/13", "amount" : 25.30},
+            {"date" : "07/30/13", "amount" : 17.30},
+            {"date" : "06/30/13", "amount" : 20.30},
+            {"date" : "05/30/13", "amount" : 5.30}]
 
- 
+var points = function() {
+  return "20,20 40,25 60,40 80,120 120,140 200,180";
+};
+
+var update_points = function () {
+  $('#graph polyline').attr('points', points());
+}
+
+$(update_points)
