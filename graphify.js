edda4243e2b0d33d65ea336897cdb4c73e479687
Kristen Sundquist <kristen@csc8.com>
2013-10-27 21:19:11 -0700

    Add working dynamically creating dvg polyline

--- graphify.js
+++ graphify.js
@@ -6,21 +6,18 @@ function Graphify (selector, data, options) {
 }
 
 Graphify.prototype.points = function() {
-
-
   return "20,20 40,25 60,40 80,120 120,140 200,180";
 };
 
 Graphify.prototype.render = function () {
-//  <svg id="graph" xmlns="http://www.w3.org/2000/svg" version="1.1">
-//    <polyline points=""
-//    style="fill:none;stroke:black;stroke-width:3" />
-//  </svg>
-
-
-  var svg = document.createElement("svg")
-  $(this.selector).append(svg)
-//      .attr('points', graph.points());
+  var svgDocument = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgDocument.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+  svgDocument.setAttribute('version', 1.1);
+  var polyline = document.createElementNS("http://www.w3.org/2000/svg", 'polyline');
+  polyline.setAttribute('points', this.points());
+  polyline.setAttribute("style", "fill:none;stroke:black;stroke-width:3");
+  svgDocument.appendChild(polyline);
+  $(this.selector).append(svgDocument);
 }
 
 
