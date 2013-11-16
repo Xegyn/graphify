e4a25423bf216132c4e3083aed17b8d11461229d
Carlo Stearns <carlo.stearns@gmail.com>
2013-11-15 23:02:40 -0800

    Added date/amount translation to x/y points

--- graphify.js
+++ graphify.js
@@ -7,40 +7,11 @@ function Graphify (selector, data, options) {
   this.height = 200;
 }
 
-Graphify.prototype.points = function () {
-
-  this.separateData();
-
-  console.log(this.xs);
-  console.log(this.ys);
-
-  var length = this.xs.length;
-  var xdist = this.width / length;
-  console.log(xdist);
-  var ydist = this.height / Math.max.apply(null, this.ys);
-  console.log(ydist);
-  var points = "";
-
-  for(var i=0; i<length; i++) {
-    points += xdist * i + "," + (this.height / this.ys[i]) + " ";
-  }
-
-  console.log(points);
-  return points;
-}
-
-Graphify.prototype.separateData = function () {
-  this.xs = new Array();
-  this.ys = new Array();
-  for(var i=0; i<this.data.length; i++) {
-    this.xs[i] = this.data[i]["date"];
-    this.ys[i] = this.data[i]["amount"];
-  }
-}
-
 Graphify.prototype.render = function () {
   this.createSvg();
   this.renderAxis();
+  this.initializeData();
+  this.translatePoints();
   this.renderLineGraph();
 }
 
@@ -68,9 +39,56 @@ Graphify.prototype.renderLine = function (x1, y1, x2, y2) {
 
 Graphify.prototype.renderLineGraph = function () {
   var polyline = document.createElementNS(this.xmlns, 'polyline');
-  polyline.setAttribute('points', this.points());
+  polyline.setAttribute('points', this.pointsForAttribute());
   polyline.setAttribute("style", "fill:none; stroke:black; stroke-width:2");
   this.svgDocument.appendChild(polyline);
+}
+
+Graphify.prototype.pointsForAttribute = function () {
+  var points = "";
+
+  for(var i=0; i<this.data.length; i++) {
+    points += this.translated_x_points[i] + "," + this.invertYAxis(this.translated_y_points[i]) + " ";
+  }
+
+  return points;
+}
+
+Graphify.prototype.invertYAxis = function (y_value) {
+  return this.height - y_value
+}
+
+Graphify.prototype.initializeData = function () {
+  this.x_values = new Array();
+  this.y_values = new Array();
+  for(var i=0; i<this.data.length; i++) {
+      this.x_values[i] = this.dateToInteger(this.data[i]["date"]);
+      this.y_values[i] = this.data[i]["amount"];
+  }
+  this.min_x = Math.min.apply(null, this.x_values)
+  this.max_x = Math.max.apply(null, this.x_values)
+  this.min_y = Math.min.apply(null, this.y_values)
+  this.max_y = Math.max.apply(null, this.y_values)
+
+}
+
+Graphify.prototype.dateToInteger = function(date) {
+  return new Date(date).valueOf();
+}
+
+Graphify.prototype.translatePoints = function() {
+  this.translated_x_points = new Array();
+  this.translated_y_points = new Array();
+
+  x_divisor = (this.max_x - this.min_x) / this.width
+  y_divisor = (this.max_y - this.min_y) / this.height
+
+  for(var i=0; i<this.data.length; i++) {
+    this.translated_x_points[i] = (this.x_values[i] - this.min_x) / x_divisor
+    this.translated_y_points[i] = (this.y_values[i] - this.min_y) / y_divisor
+  }
+  console.log(this.translated_x_points);
+  console.log(this.translated_y_points);
 }
 
 
@@ -78,12 +96,12 @@ Graphify.prototype.renderLineGraph = function () {
 //----------------------------------------------------
 
 var create_graph = function () {
-  var data = [{"date" : "10/30/13", "amount" : 12.30},
-    {"date" : "09/30/13", "amount" : 14.30},
-    {"date" : "08/30/13", "amount" : 25.30},
-    {"date" : "07/30/13", "amount" : 17.30},
-    {"date" : "06/30/13", "amount" : 20.30},
-    {"date" : "05/30/13", "amount" : 5.30}]
+  var data = [{"date" : "05/30/13", "amount" : 12.30},
+    {"date" : "06/30/13", "amount" : 14.30},
+    {"date" : "07/30/13", "amount" : 25.30},
+    {"date" : "08/30/13", "amount" : 17.30},
+    {"date" : "09/30/13", "amount" : 50.30},
+    {"date" : "10/30/13", "amount" : 5.30}]
   var graph = new Graphify("div#graph", data, {});
   graph.render();
 };
